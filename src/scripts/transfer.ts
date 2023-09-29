b43cc8971e2d6081d99a5f2c232507f49049fa00
Xueying Wang <wang.xy.326@gmail.com>
2023-09-29 16:49:04 +0200

    update transfer script

--- src/scripts/transfer.ts
+++ src/scripts/transfer.ts
@@ -2,28 +2,53 @@
 
 import { EventEmitter } from 'node:events';
 
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 import chalk from 'chalk';
 import { Keyring } from '@polkadot/keyring';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { AssetTransferApi, constructApiPromise, TxResult } from '@substrate/asset-transfer-api';
+
+import { txCallback } from '../utils/index.js';
 
 const eventEmitter = new EventEmitter();
 
-const main = async () => {
-  const { api, specName, safeXcmVersion } = await constructApiPromise('ws://127.0.0.1:9910');
-  console.log('spec name:', specName);
+const main = async ({
+  url,
+  seed,
+  dest,
+  recipient,
+  assets,
+  amounts,
+  version
+} : {
+  url: string,
+  seed: string,
+  dest: string,
+  recipient: string,
+  assets: string[],
+  amounts: string[],
+  version?: number
+}) => {
+  await cryptoWaitReady();
+  const keyring = new Keyring({ type: 'sr25519' });
+  const signer = keyring.addFromUri(seed);
+
+  console.log(chalk.yellow(`Initializing API with ${url}`));
+  const { api, specName, safeXcmVersion } = await constructApiPromise(url);
   const assetApi = new AssetTransferApi(api, specName, safeXcmVersion);
 
   let submittableTx: TxResult<'submittable'>;
   try {
     submittableTx = await assetApi.createTransferTransaction(
-      '2000',
-      '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', // Alice
-      ['1984'],
-      ['1500000000000'],
+      dest,
+      recipient,
+      assets,
+      amounts,
       {
         format: 'submittable',
         isLimited: true,
-        xcmVersion: 3,
+        xcmVersion: version || safeXcmVersion,
       }
     );
 
@@ -35,42 +60,77 @@ const main = async () => {
     throw Error(e as string);
   }
 
-  // const decoded = assetApi.decodeExtrinsic(submittableTx.tx.toString(), 'submittable');
-  // console.log(`\n${PURPLE}The following decoded tx:\n${GREEN} ${JSON.stringify(JSON.parse(decoded), null, 4)}${RESET}`);
-
-  // Actually send the tx...
-  const keyring = new Keyring({ type: 'sr25519' });
-  const alice = keyring.addFromUri('//Alice');
-
-  await submittableTx.tx.signAndSend(alice, ({ status, dispatchError, internalError }) => {
-    console.log(chalk.cyan('Transaction status: ', status));
-    if (status.isInBlock || status.isFinalized) {
-      let errorMessage: string | undefined;
-      if (dispatchError) {
-        if (dispatchError.isModule) {
-          const decoded = api.registry.findMetaError(dispatchError.asModule);
-          const { docs, method, section } = decoded;
-
-          errorMessage = `${section}.${method}: ${docs.join(' ')}`;
-        } else {
-          // Other, CannotLookup, BadOrigin, no extra info
-          errorMessage = dispatchError.toString();
-        }
-      }
-      if (internalError) {
-        errorMessage = internalError.message;
-      }
-      if (status.isFinalized) {
-        eventEmitter.emit('txFinalized', errorMessage);
-      }
-    }
-  });
+  await submittableTx.tx.signAndSend(signer, txCallback(api, 'txFinalized', eventEmitter));
 };
 
-main().finally(() => {
-  eventEmitter.on('txFinalized', (errorMessage?: string) => {
+const argv = yargs(hideBin(process.argv))
+  .command('Usage: $0 <url> [options]', 'Transfer assets', (args) => {
+    args.positional('url', {
+      describe: 'The RPC endpoint URL',
+      type: 'string'
+    });
+  })
+  .example(
+    '$0 ws://127.0.0.1:9944 -s //Alice -d 2000 -r 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY -a 1984 -m 1500000000000',
+    'Connects to network and transfers 1500000000000 units of asset with ID 1984 to recipient account on parachain 2000.'
+  )
+  .option('seed', {
+    type: 'string',
+    alias: 's',
+    describe: 'The account seed to use for making transactions',
+    default: '//Alice',
+  })
+  .option('dest', {
+    type: 'string',
+    alias: 'd',
+    describe: 'ID of the destination chain',
+    requiresArg: true,
+  })
+  .option('recipient', {
+    type: 'string',
+    alias: 'r',
+    describe: 'Address of recipient account',
+    requiresArg: true,
+  })
+  .option('assets', {
+    type: 'string',
+    alias: 'a',
+    describe: 'Array of assetId\'s to be transferred',
+    requiresArg: true,
+    array: true
+  })
+  .option('amounts', {
+    type: 'string',
+    alias: 'm',
+    describe: 'Array of the amounts of each token to transfer',
+    requiresArg: true,
+    array: true
+  })
+  .option('xcmVersion', {
+    type: 'number',
+    alias: 'v',
+    describe: 'XCM version to use',
+  })
+  .help('h')
+  .alias('h', 'help')
+  .scriptName('transfer')
+  .argv as any;
+
+main({
+  url: argv._[0],
+  seed: argv.seed,
+  dest: argv.dest,
+  recipient: argv.recipient,
+  assets: argv.assets,
+  amounts: argv.amounts,
+  version: argv.version
+}).finally(() => {
+  eventEmitter.on('txFinalized', (errorMessage?: string, polkadotXcmError?: string) => {
+    if (polkadotXcmError) {
+      console.log(chalk.red('Error in XCM execution:', polkadotXcmError));
+    }
     if (errorMessage) {
-      console.error('Error in extrinsic:', errorMessage);
+      console.log(chalk.red('Error in extrinsic:', errorMessage));
     } else {
       console.log(chalk.green('Extrinsic success'));
     }
--- src/utils/extrinsic.ts
+++ src/utils/extrinsic.ts
@@ -1,36 +1,74 @@
-// import '@polkadot/api-augment';
+import '@polkadot/api-augment';
 
 import { EventEmitter } from 'node:events';
 
 import { ApiPromise } from '@polkadot/api';
 import type { ISubmittableResult } from '@polkadot/types/types';
+import type { Outcome } from '@polkadot/types/interfaces/xcm';
 import chalk from 'chalk';
+
+function getExtrinsicError(api: ApiPromise, result: ISubmittableResult) {
+  let errorMessage: string | undefined;
+  const { dispatchError, internalError } = result;
+  if (dispatchError) {
+    if (dispatchError.isModule) {
+      const decoded = api.registry.findMetaError(dispatchError.asModule);
+      const { docs, method, section } = decoded;
+
+      const moduleError = `Module Error: ${section}.${method}: ${docs.join(' ')}`;
+      errorMessage = errorMessage ? errorMessage + moduleError : moduleError;
+    } else {
+      // Other, CannotLookup, BadOrigin, no extra info
+      errorMessage = errorMessage ? errorMessage + dispatchError.toString() : dispatchError.toString();
+    }
+  }
+  if (internalError) {
+    const ie = `Internal Error: ${internalError.message}`;
+    errorMessage = errorMessage ? errorMessage + ie : ie;
+  }
+  return errorMessage;
+}
+
+function extractPolkadotXcmError(api: ApiPromise, { events }: ISubmittableResult) {
+  return events
+    .filter(({ event }) => api.events.polkadotXcm.Attempted.is(event))
+    .map(({
+      event: {
+        data: [outcome],
+      },
+    }) => {
+      const o = outcome as Outcome;
+      if (o.isIncomplete) {
+        const [_, err] = o.asIncomplete;
+        return err.type.toString();
+      }
+      if (o.isError) {
+        return o.asError.type.toString();
+      }
+      return undefined;
+    });
+}
 
 export const txCallback = (api: ApiPromise, name = '', eventEmitter: EventEmitter) => {
   return (result: ISubmittableResult) => {
-    const { status, dispatchError, internalError } = result;
+    const { status } = result;
 
     console.log(chalk.cyan(`[${name}] Transaction status: ${status}`));
 
-    if (status.isInBlock || status.isFinalized) {
-      let errorMessage: string | undefined;
-      if (dispatchError) {
-        if (dispatchError.isModule) {
-          const decoded = api.registry.findMetaError(dispatchError.asModule);
-          const { docs, method, section } = decoded;
-
-          errorMessage = `${section}.${method}: ${docs.join(' ')}`;
-        } else {
-          // Other, CannotLookup, BadOrigin, no extra info
-          errorMessage = dispatchError.toString();
+    if (status.isFinalized) {
+      const extrinsicError = getExtrinsicError(api, result);
+      let polkadotXcmError: string | undefined;
+      // pallet_xcm errors are not emitted as module errors so we need to decode the events to extract errors
+      if (api.events.polkadotXcm) {
+        const es = extractPolkadotXcmError(api, result);
+        for (const e of es) {
+          if (e) {
+            polkadotXcmError = polkadotXcmError ? polkadotXcmError + `\nPolkadotXcm.Attempted Error: ${e}` : e;
+          }
         }
       }
-      if (internalError) {
-        errorMessage = internalError.message;
-      }
-      if (status.isFinalized) {
-        eventEmitter.emit(name, errorMessage);
-      }
+
+      eventEmitter.emit(name, extrinsicError, polkadotXcmError);
     }
   };
 };
