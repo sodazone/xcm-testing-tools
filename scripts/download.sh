84a649ee042aeb912ae9e7f0dc2e50c18bc22dda
Xueying Wang <wang.xy.326@gmail.com>
2024-02-21 16:04:39 +0100

    update download script to get polkadot v1.7.0 binaries

--- scripts/download.sh
+++ scripts/download.sh
@@ -13,10 +13,10 @@ test $(uname -m) = "x86_64" || {
 }
 
 test -f zombienet || wget https://github.com/paritytech/zombienet/releases/download/v1.3.92/zombienet-linux-x64 -O zombienet
-test -f polkadot || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.4.0/polkadot -O polkadot
-test -f polkadot-parachain || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.4.0/polkadot-parachain -O polkadot-parachain
-test -f polkadot-execute-worker || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.4.0/polkadot-execute-worker -O polkadot-execute-worker
-test -f polkadot-prepare-worker || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.4.0/polkadot-prepare-worker -O polkadot-prepare-worker
+test -f polkadot || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.7.0/polkadot -O polkadot
+test -f polkadot-parachain || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.7.0/polkadot-parachain -O polkadot-parachain
+test -f polkadot-execute-worker || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.7.0/polkadot-execute-worker -O polkadot-execute-worker
+test -f polkadot-prepare-worker || wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.7.0/polkadot-prepare-worker -O polkadot-prepare-worker
 test -f astar-collator || { 
   wget https://github.com/AstarNetwork/Astar/releases/download/v5.30.0/astar-collator-v5.30.0-ubuntu-x86_64.tar.gz -O astar-collator.tgz
   tar xzf astar-collator.tgz
